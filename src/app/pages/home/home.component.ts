2bea91ec5bf48a93018c0fbf1b8ad76703eef735
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-12-03 13:15:35 -0800

    feat: Add transition in more info button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
+  @ViewChild('myInvestInfo', { static: false }) myInvest!: ElementRef<HTMLDivElement>
+  @ViewChild('myInvestMoreInfoButton', { static: true }) myInvestMoreInfoButton!: ElementRef<HTMLDivElement>
 
+  showMoreInfo(option: string): void {
+    if (option == 'myInvest') {
+      this.myInvest.nativeElement.classList.toggle('active');
+      this.myInvestMoreInfoButton.nativeElement.classList.toggle('more-info-button-activated');
+
+      const currentText = this.myInvestMoreInfoButton.nativeElement.textContent;
+      if (currentText === 'mais informações') {
+        this.myInvestMoreInfoButton.nativeElement.textContent = 'fechar informações';
+      } else {
+        this.myInvestMoreInfoButton.nativeElement.textContent = 'mais informações';
+      }
+    }
+  }
 }
